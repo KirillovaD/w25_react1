6c38696f62f79d335555d3c853827cfae2a1a995
KirillovaD <sdaria@inbox.ru>
2022-10-06 14:04:39 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Знания", "Знания к Требованиям"],
+  ["HTML", 9],
+  ["CSS", 8],
+  ["JavaScript", 4],
+  ["React", 0.1],
+];
+
+export const options = {
+  title: "Я и фронтенд",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"100%"}
+        height={"400px"}
+      />
+
     </div>
   );
 }
